e8a3e7941a10c2e5ad47f7818b13c8d91f020a55
xurxogonzalez <info@webferrol.com>
2021-10-16 17:56:36 +0200

    js. Añadido element.style.left

--- js/app.js
+++ js/app.js
@@ -17,28 +17,35 @@ const getIndex = () => {
     );
 }
 const avanzar = (e) => {
-    
+
     let index = getIndex();
-    if (index < itemsImg.length-1) {
+    if (index < itemsImg.length - 1) {
         itemsImg[index].classList.remove('visible');
+
+
         itemsImg[index].nextElementSibling.classList.add('visible');
+        itemsImg[index].nextElementSibling.style.left = `0`;
     }
     console.log(getIndex())
-    if(getIndex() === itemsImg.length-1)
+    if (getIndex() === itemsImg.length - 1)
         e.target.classList.add("btn-hidden");
     else
         btnPrevious.classList.remove("btn-hidden");
 }
 
 const retroceder = (e) => {
-    
+
     let index = getIndex();
+    let tam = 100;
     if (index > 0) {
+        tam = index * tam;
         itemsImg[index].classList.remove('visible');
+        itemsImg[index].style.left = `-${tam}%`;
         itemsImg[index].previousElementSibling.classList.add('visible');
+
     }
     console.log(getIndex())
-    if(getIndex() === 0)
+    if (getIndex() === 0)
         e.target.classList.add("btn-hidden");
     else
         btnNext.classList.remove("btn-hidden");
